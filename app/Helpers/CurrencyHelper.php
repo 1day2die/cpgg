b5223f18221f6cf913e978c9b828560bb0205260
LakshmiBhaskarPVL <16p3034@kennedyglobalschool.edu.in>
2025-10-29 17:25:37 +0530

    feat: add currency format override functionality and update related components

--- app/Helpers/CurrencyHelper.php
+++ app/Helpers/CurrencyHelper.php
@@ -11,9 +11,35 @@ class CurrencyHelper
         return $amount / 1000;
     }
 
-    public function formatForDisplay($amount, $decimals = 2)
+    public function formatForDisplay($amount, $decimals = 2, $locale = null, $ignoreOverride = false)
     {
-        return number_format($this->convertForDisplay($amount), $decimals, ',', '.');
+        $locale = $locale ?: str_replace('_', '-', app()->getLocale());
+
+        if (!$ignoreOverride) {
+            $override = resolve(\App\Settings\GeneralSettings::class)->currency_format_override ?? null;
+            if ($override) {
+                $locale = $override;
+            }
+        }
+
+        $display = $this->convertForDisplay($amount);
+
+        if ($locale === 'bg' && $display <= 9999) {
+            return number_format($display, $decimals, ',', '');
+        }
+
+        if ($locale === 'es' && $display < 10000) {
+            return number_format($display, $decimals, ',', '');
+        }
+
+        if ($locale === 'pl' && $display <= 9999) {
+            return number_format($display, $decimals, ',', '');
+        }
+
+        $formatter = new NumberFormatter($locale, NumberFormatter::DECIMAL);
+        $formatter->setAttribute(NumberFormatter::MIN_FRACTION_DIGITS, $decimals);
+        $formatter->setAttribute(NumberFormatter::MAX_FRACTION_DIGITS, $decimals);
+        return $formatter->format($display);
     }
 
     public function formatForForm($amount, $decimals = 2)
@@ -29,6 +55,12 @@ class CurrencyHelper
     public function formatToCurrency(int $amount, $currency_code, $locale = null,)
     {
         $locale = $locale ?: str_replace('_', '-', app()->getLocale());
+
+        // overriding users locale with global override
+        $override = resolve(\App\Settings\GeneralSettings::class)->currency_format_override ?? null;
+        if ($override) {
+            $locale = $override;
+        }
 
         $formatter = new NumberFormatter($locale, NumberFormatter::CURRENCY);
 
